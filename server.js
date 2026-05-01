a0e4705c099b95179d30d0c708f93ea48e64a465
shubhamkotak2005 <shubham.kotak09@gmail.com>
2026-05-01 05:57:48 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("This is the home page");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
